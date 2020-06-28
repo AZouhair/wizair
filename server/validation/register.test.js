d0f99050b48c1bfab728e3c0bf15542255d35ed7
amine <m.amine.zouhair@gmail.com>
2020-06-28 21:05:55 +0200

    message

---
+++ server/validation/register.test.js
@@ -0,0 +1,7 @@
+const register = require("./c:\\Users\\mamin\\Desktop\\wizair_app\\server\\validation\\register")
+// @ponicode
+describe("register", () => {
+    test("0", () => {
+        register(undefined)
+    })
+})
